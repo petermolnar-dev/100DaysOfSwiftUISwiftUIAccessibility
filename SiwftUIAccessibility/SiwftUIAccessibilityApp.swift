3973dde126c9b51f536d3a098f799fbd728344dc
Peter Molnar <petermolnar.hu@gmail.com>
2022-07-31 20:31:49 +0200

    Initial Commit

---
+++ SiwftUIAccessibility/SiwftUIAccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiwftUIAccessibilityApp.swift
+//  SiwftUIAccessibility
+//
+//  Created by Peter Molnar on 31/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SiwftUIAccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
